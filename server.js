fed260a8e65719378bfc15c2ee2689e3e633567d
Conliffe99 <122856513+Conliffe99@users.noreply.github.com>
2023-03-24 21:41:48 -0500

    commit 3

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const express = require (`express`);
 const drinks = require ('./models/drinks');
 const app = express ();
+const drinks = require("./models/drinks");
+const foods = require("./models/food");
+app.set('view engine', 'ejs');
 const PORT = 3000;
 
+app.get('/drinks', (req, res) => {
+    res.render("drinks_index", {drinks});
+})
 
+app.get('/drinks/:id', (req, res) => {
+    let drink = drinks[req.params.id];
+    res.render("drinks_show", {drink});
+})
 
+app.get('/foods', (req, res) => {
+    res.render("food_index", {foods});
+})
+
+app.get('/foods/:id', (req, res) => {
+    let food = foods[req.params.id];
+    res.render("food_show", {food});
+})
 
 app.listen(3000, () => {
     console.log('Listening on port 3000!');
